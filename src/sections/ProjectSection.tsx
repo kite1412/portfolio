38023c61799135a14162c03d2a62f45b932648a1
kite1412 <frozradit@gmail.com>
2025-06-09 15:53:35 +0700

    feat: add other project section

--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -12,8 +12,16 @@ import Ktor from "../assets/ktor.svg?react";
 import GitHub from "../assets/github.svg?react";
 import Dart from "../assets/dart.svg?react";
 import Flutter from "../assets/flutter.svg?react";
+import Express from "../assets/Express.svg?react";
 import PersonCircle from "../assets/person-circle.svg?react";
 import About from "../assets/about.svg?react";
+import ReactLogo from "../assets/react.svg?react";
+import JavaScript from "../assets/javascript.svg?react";
+import Tailwind from "../assets/tailwind.svg?react";
+import kiyayKoki from "../assets/kiyay-koki-logo.svg";
+import movieHub from "../assets/moviehub-logo.svg";
+import csirt from "../assets/csirt-logo.svg";
+import books from "../assets/books.svg";
 
 export default function ProjectSection({
   ref
@@ -22,20 +30,40 @@ export default function ProjectSection({
 }) {
   return (
     <SectionLayout ref={ref} section={projectSection.displayName}>
-      <div className="flex flex-col gap-20 px-20">
-        {projects.map((p, i) => (
-          <ProjectDetail
-            name={p.name}
-            desc={p.desc}
-            img={p.img}
-            role={p.role}
-            type={p.type}
-            techImgs={p.techImgs}
-            githubRepo={p.githubRepo}
-            reverse={i % 2 === 1}
-            coverType={p.coverType}
-          />
-        ))}
+      <div className="flex flex-col gap-30">
+        <div className="flex flex-col gap-20 px-20">
+          {featuredProjects.map((p, i) => (
+            <ProjectDetail
+              name={p.name}
+              desc={p.desc}
+              img={p.img}
+              role={p.role}
+              type={p.type}
+              techImgs={p.techImgs}
+              githubRepo={p.githubRepo}
+              reverse={i % 2 === 1}
+              coverType={p.coverType}
+            />
+          ))}
+        </div>
+        <SectionLayout ref={ref} section="Other Project" ignorePadding={true}>
+          <div className="grid grid-cols-2 gap-8">
+            {
+              otherProjects.map(p => (
+                <ProjectDetailSmall
+                  name={p.name}
+                  desc={p.desc}
+                  img={p.img}
+                  role={p.role}
+                  type={p.type}
+                  techImgs={p.techImgs}
+                  githubRepo={p.githubRepo}
+                  coverClassName={p.coverClassName}
+                />
+              ))
+            }
+          </div>
+        </SectionLayout>
       </div>
     </SectionLayout>
   );
@@ -50,9 +78,11 @@ function ProjectDetail({
   techImgs,
   githubRepo,
   reverse = false,
-  coverType = "object-fit"
+  coverType = "object-fill",
+  coverClassName = ""
 }: Project & {
   reverse?: boolean;
+  coverClassName?: string;
 }) {
   const reverseText = reverse ? "text-end" : "text-start";
   const infoIcon = "size-[18px]";
@@ -61,7 +91,7 @@ function ProjectDetail({
     <div className={`flex gap-6 w-full ${reverse && "flex-row-reverse"}`}>
       <img
         src={img}
-        className={`w-[45%] h-fit aspect-[3/2] object-fit rounded-[16px] ${coverType}`}
+        className={`${coverClassName || "w-[45%] h-fit aspect-[3/2] rounded-[16px]"} ${coverType}`}
       />
       <div
         className={`
@@ -88,16 +118,42 @@ function ProjectDetail({
             <div className="text-background">{t}</div>
           ))}
         </div>
-        <div
-          className="flex gap-2 cursor-pointer select-none items-center"
-          onClick={() => window.open(githubRepo, "_blank")}
-        >
-          <GitHub className="size-[22px]" />
-          GitHub Repo
-        </div>
+        {
+          githubRepo && <div
+            className="flex gap-2 cursor-pointer select-none items-center"
+            onClick={() => window.open(githubRepo, "_blank")}
+          >
+            <GitHub className="size-[22px]" />
+            GitHub Repo
+          </div>
+        }
       </div>
     </div>
   );
+}
+
+function ProjectDetailSmall({
+  name,
+  desc,
+  img,
+  role,
+  type,
+  techImgs,
+  githubRepo,
+  coverClassName = ""
+}: Project & {
+  coverClassName?: string;
+}) {
+  return <ProjectDetail 
+    name={name}
+    desc={desc}
+    img={img}
+    role={role}
+    type={type}
+    techImgs={techImgs}
+    githubRepo={githubRepo}
+    coverClassName={`size-[60px] rounded-[8px] ${coverClassName}`}
+  />;
 }
 
 function ProjectInfo({
@@ -119,7 +175,7 @@ function ProjectInfo({
 
 const icon = "size-[24px]";
 
-const projects: Array<Project> = [
+const featuredProjects: Array<Project> = [
   {
     name: "Taskify",
     desc: "A fully functional Android task management application that delivers reliability, ease of use, and an intuitive design to enhance productivity.",
@@ -164,6 +220,67 @@ const projects: Array<Project> = [
     ],
     githubRepo: "https://github.com/kite1412/flutter-mock-mal",
     coverType: "object-cover"
+  }
+];
+
+const coverClassName = "p-2";
+
+const otherProjects: Array<Project & { coverClassName?: string; }> = [
+  {
+    name: "Kiyay Koki",
+    desc: "An e-commerce platform for selling goldfish, along with fish feed and aquarium supplies.",
+    img: kiyayKoki,
+    role: "Frontend Dev",
+    type: "Team project",
+    techImgs: [
+      <JavaScript className={icon} />,
+      <ReactLogo className={icon} />,
+      <Tailwind className={icon} />
+    ],
+    githubRepo: "https://github.com/kite1412/react-kiyay-koki",
+    coverClassName: `bg-black ${coverClassName}`
+  },
+  {
+    name: "MovieHub",
+    desc: "A web application for discovering movies using the TMDB API.",
+    img: movieHub,
+    role: "Frontend Dev",
+    type: "Team project",
+    techImgs: [
+      <JavaScript className={icon} />,
+      <ReactLogo className={icon} />
+    ],
+    githubRepo: "https://github.com/kite1412/react-moviehub",
+    coverClassName: `bg-[#DA70D6] ${coverClassName}`
+  },
+  {
+    name: "CSIRT System",
+    desc: "A web-based CSIRT system designed to facilitate efficient incident tracking, team collaboration, and response management for cybersecurity operations.",
+    img: csirt,
+    role: "Fullstack Dev",
+    type: "Team project",
+    techImgs: [
+      <JavaScript className={icon} />,
+      <ReactLogo className={icon} />,
+      <Tailwind className={icon} />,
+      <Express className={icon} />
+    ],
+    coverClassName: `bg-[#081423] ${coverClassName}`
+  },
+  {
+    name: "School LMS",
+    desc: "A Learning Management System that enables daily schedule display, classroom material sharing, assignment management, and student attendance tracking.",
+    img: books,
+    role: "Fullstack Dev",
+    type: "Team project",
+    techImgs: [
+      <JavaScript className={icon} />,
+      <ReactLogo className={icon} />,
+      <Tailwind className={icon} />,
+      <Express className={icon} />
+    ],
+    githubRepo: "https://github.com/kite1412/react-lms",
+    coverClassName: `bg-white ${coverClassName}`
   }
 ];
 
